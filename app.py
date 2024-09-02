f576eea832531ee0d0d298a20e6f9bfe492a5a71
Sambi-Singh <sambisingh289@gmail.com>
2024-09-01 21:07:13 -0500

    Read from CSV file 1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 # Day 1 Aug.31, 2024 - Setting up environment and git environment.
+
+import streamlit as st
+import pandas as pd
+import datatime
+import plotly.express as px
+import plotly.graph_objects as go
+
+# read the data from cvs file
+data_file = pd.read_csv("/E-COMMERCE DATA/business.retailsales.csv")
